82d06739f9371edcce823c7e818e73d1afaafc2c
Nisha R S <nisha021rs@gmail.com>
2026-03-21 09:05:48 +0530

    performed marks grading system in conditional statement

--- conditional_statements.py
+++ conditional_statements.py
@@ -1,9 +1,20 @@
-colour = (input("Enter colour : "))
-if(colour == "red"):
-  print("Stop")
-elif(colour == "yellow"):
-  print("Wait")
-elif(colour == "green"):
-  print("Go")
+# colour = (input("Enter colour : "))
+# if(colour == "red"):
+#   print("Stop")
+# elif(colour == "yellow"):
+#   print("Wait")
+# elif(colour == "green"):
+#   print("Go")
+# else:
+#   print("Light is broken")
+
+marks = int(input("Enter marks : "))
+
+if(marks >= 95):
+ print("Grade : A ")
+elif(marks <= 95 and marks >= 80):
+ print("Grade : B ")
+elif(marks <= 80 and marks >= 70):
+ print("Grade : C ")
 else:
-  print("Light is broken")
+ print("Fail")
