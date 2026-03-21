791aaa9cd4be771b41d741706424ffa984864642
Nisha R S <nisha021rs@gmail.com>
2026-03-21 08:51:28 +0530

    performed traffic lights condition in conditional statement

--- 1.py
+++
@@ -1,6 +0,0 @@
-name=input("Name : ") #string input
-print(name)
-a=int(input(("Age : "))) #integer input
-print(a)
-b=float(input("Price :")) #float input
-print(b)
---
+++ conditional_statements.py
@@ -0,0 +1,9 @@
+colour = (input("Enter colour : "))
+if(colour == "red"):
+  print("Stop")
+elif(colour == "yellow"):
+  print("Wait")
+elif(colour == "green"):
+  print("Go")
+else:
+  print("Light is broken")
